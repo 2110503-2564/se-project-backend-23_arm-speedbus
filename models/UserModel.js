65692f9c62271b3eae470549eb64839a35bdca44
Rathanon Makaramanee <rathanon.makaramanee@gmail.com>
2025-02-25 23:09:21 +0700

    Removed address in user's schema.

--- models/UserModel.js
+++ models/UserModel.js
@@ -19,10 +19,6 @@ const UserSchema=new mongoose.Schema({
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,'Please add a valid email'
         ]
     },
-    address:{
-        type:String,
-        required:[true,'Please add an address'],
-    },
     role:{
     type:String,
         enum:['user','admin'],
