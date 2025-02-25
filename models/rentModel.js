395da361a0990a3eb8be9f0e1981a7928e877b6f
Rathanon Makaramanee <rathanon.makaramanee@gmail.com>
2025-02-25 15:40:43 +0700

    Added RentModel.

---
+++ models/rentModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const RentSchema = new mongoose.Schema({
+    car_id: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'CarModel', 
+        required: true 
+    },
+    user_id: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'UserModel', 
+        required: true 
+    }, 
+    iDate: { 
+        type: Date, 
+        default: Date.now 
+    }, 
+    startDate: { 
+        type: Date, 
+        required: true 
+    }, 
+    endDate: { 
+        type: Date, 
+        required: true 
+    } 
+});
+
+module.exports = mongoose.model('Rent', RentSchema);
