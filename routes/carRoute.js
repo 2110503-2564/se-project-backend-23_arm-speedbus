31ced9c02d191da4a61605eb9aa1d2bd3c7a4c2a
Rathanon Makaramanee <rathanon.makaramanee@gmail.com>
2025-02-28 03:09:01 +0700

    Mount another router for finding rents for carId.

--- routes/carRoute.js
+++ routes/carRoute.js
@@ -2,7 +2,11 @@ const express = require('express');
 const { getCars, getCar, createCar, updateCar, deleteCar} = require('../controllers/carController');
 const {protect, authorize} = require('../middleware/auth');
 
+const rentRouter = require('./rentRoute');
+
 const router = express.Router();
+
+router.use('/:carId/rents',rentRouter);
 
 router.route('/').get(getCars).post(protect, authorize('admin'), createCar);
 router.route('/:id').get(getCar).put(protect, authorize('admin'), updateCar).delete(protect, authorize('admin'), deleteCar);
