da198ce788f3daf6948d245597703bbef9268dd9
Rathanon Makaramanee <rathanon.makaramanee@gmail.com>
2025-03-25 14:55:25 +0700

    add things for vercel.

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/rents',rentRoute);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${process.env.HOST}: ${PORT}`));
 
 //handle unhandeled promise rejections
 process.on('unhandledRejection', (err, promise) => {
