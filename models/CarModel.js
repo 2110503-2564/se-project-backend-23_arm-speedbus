622c5a68f7204fbbdd9f4f4d63689593246da52b
Baworntatt Taeprasert <baworntatt.t@gmail.com>
2025-04-09 10:26:49 +0700

    change car's model to description

--- models/CarModel.js
+++ models/CarModel.js
@@ -1,47 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const CarSchema = new mongoose.Schema({
-    name: { 
-        type: String, 
-        required: true 
+const CarSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    vin_plate: { 
-        type: String, 
-        required: true, 
-        unique: true 
+    vin_plate: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    provider_info: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Provider', 
-        required: true 
+    provider_info: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Provider",
+      required: true,
     },
     picture: {
-        type: String, 
-        required: true
+      type: String,
+      required: true,
     },
-    capacity: { 
-        type: Number, 
-        required: true 
+    capacity: {
+      type: Number,
+      required: true,
     },
-    model: { 
-        type: String, 
-        required: true 
+    description: {
+      type: String,
+      required: true,
     },
-    pricePerDay: { 
-        type: Number, 
-        required: true 
+    pricePerDay: {
+      type: Number,
+      required: true,
     },
-
-    }, { 
+  },
+  {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
+  }
+);
+
+CarSchema.virtual("Provider", {
+  ref: "ProviderModel",
+  localField: "provider_id",
+  foreignField: "_id",
+  justOne: false,
 });
 
-CarSchema.virtual('Provider', {
-    ref: 'ProviderModel',
-    localField: 'provider_id',
-    foreignField: '_id',
-    justOne: false
-});
-
-module.exports = mongoose.model('Car', CarSchema);
+module.exports = mongoose.model("Car", CarSchema);
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const connectDB = require('./config/db');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const {xss} = require('express-xss-sanitizer');
-const rateLimit=require('express-rate-limit');
-const hpp = require('hpp');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const { xss } = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
-
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
 const app = express();
 
 const limiter = rateLimit({
-    windowMs:10*60*1000,//10 mins
-    max:1000
+  windowMs: 10 * 60 * 1000, //10 mins
+  max: 1000,
 });
 
 app.use(express.json());
@@ -30,24 +29,29 @@ app.use(limiter);
 app.use(hpp());
 app.use(cors());
 
-const carRoute = require('./routes/carRoute');
-const authRoute = require('./routes/authRoute');
-const rentRoute = require('./routes/rentRoute');
-const providerRoute = require('./routes/providerRoute');
-const auditLogRoute = require('./routes/auditLogRoute');
+const carRoute = require("./routes/carRoute");
+const authRoute = require("./routes/authRoute");
+const rentRoute = require("./routes/rentRoute");
+const providerRoute = require("./routes/providerRoute");
+const auditLogRoute = require("./routes/auditLogRoute");
 
-app.use('/api/v1/auditlogs', auditLogRoute);
-app.use('/api/v1/providers', providerRoute);
-app.use('/api/v1/cars', carRoute);
-app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/rents',rentRoute);
+app.use("/api/v1/auditlogs", auditLogRoute);
+app.use("/api/v1/providers", providerRoute);
+app.use("/api/v1/cars", carRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/rents", rentRoute);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${process.env.HOST}: ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${process.env.HOST}:${PORT}`
+  )
+);
 
 //handle unhandeled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
-    //close server and exit process
-    server.close(() => process.exit(1));
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server and exit process
+  server.close(() => process.exit(1));
 });
