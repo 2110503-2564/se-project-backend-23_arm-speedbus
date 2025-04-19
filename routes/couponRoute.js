51234fe78b731af0fd017dbb6b6f05e9eb06659e
Rathanon Makaramanee <rathanon.makaramanee@gmail.com>
2025-04-19 13:20:42 +0700

    fixed.

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const {
-  getAllRewards,
-  getOneReward,
-  getMyRewards,
-  createReward,
-  updateReward,
-  deleteReward,
-} = require("../controllers/rewardController");
+  getAllCoupons,
+  getOneCoupon,
+  getMyCoupons,
+  createCoupon,
+  updateCoupon,
+  deleteCoupon,
+} = require("../controllers/couponController");
 
 const { protect, authorize } = require("../middleware/auth");
 
@@ -14,15 +14,15 @@ const router = express.Router({ mergeParams: true });
 
 router
   .route("/")
-  .get(protect, authorize("admin"), getAllRewards)
-  .post(protect, authorize("admin", "user"), createReward);
+  .get(protect, authorize("admin"), getAllCoupons)
+  .post(protect, authorize("admin", "user"), createCoupon);
 
-router.route("/user").get(protect, getMyRewards);
+router.route("/user").get(protect, getMyCoupons);
 
 router
   .route("/:id")
-  .get(protect, authorize("admin"), getOneReward)
-  .put(protect, authorize("admin", "user"), updateReward)
-  .delete(protect, authorize("admin", "user"), deleteReward);
+  .get(protect, authorize("admin"), getOneCoupon)
+  .put(protect, authorize("admin", "user"), updateCoupon)
+  .delete(protect, authorize("admin", "user"), deleteCoupon);
 
 module.exports = router;
