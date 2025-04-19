5a4b8b2b4c389d380303f1c31512fdfe9b49567b
Rathanon Makaramanee <rathanon.makaramanee@gmail.com>
2025-04-19 14:58:18 +0700

    Added a function in debug.js to initiate total payment for all users.

--- debug.js
+++ debug.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Rent = require('./models/RentModel');
+const User = require('./models/UserModel');
 const Car = require('./models/CarModel')
 const connectDB = require('./config/db');
 require('dotenv').config({path: './config/config.env'});
@@ -34,4 +35,38 @@ async function initiateTotalDaysAndPrice() {
   }
 }
 
-initiateTotalDaysAndPrice();
+async function initiateTotalPaymentForAllUsers() {
+  try {
+    await connectDB();
+    const rents = await Rent.find().populate({path: 'car_info', select: 'name vin_plate pricePerDay'});
+    const users = await User.find();
+    console.log(`Found ${rents.length} rent records`);
+    console.log(`Found ${users.length} user records`);
+
+    for (const user of users) {
+      let sum = 0;
+      for (const rent of rents){
+        if (rent.totalPrice && rent.user_info) {
+          if (rent.user_info == user._id){
+            sum += rent.totalPrice;
+          }
+        }
+        else {
+          console.log(`Skipped rent ${rent._id} due to missing data`);
+        }
+      }
+      user.totalPayment = sum;
+      await user.save();
+      console.log(`Updated rent ${rent._id}: ${rent.totalDays} days, $${rent.totalPrice}`);
+    }
+
+    await mongoose.disconnect();
+    console.log('MongoDB disconnected');
+  } catch (err) {
+    console.error('Error during update:', err);
+    process.exit(1);
+  }
+}
+
+// initiateTotalDaysAndPrice();
+initiateTotalPaymentForAllUsers()
