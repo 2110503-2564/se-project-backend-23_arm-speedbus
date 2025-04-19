4d7c53a01050309e5dbebe5eb3bc8e9070cc61c5
Rathanon Makaramanee <rathanon.makaramanee@gmail.com>
2025-04-19 20:29:28 +0700

    Added discount in RentModel.

--- models/RentModel.js
+++ models/RentModel.js
@@ -45,6 +45,10 @@ const RentSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    discount: {
+        type: Number,
+        required: true,
+    },
     status: {
       type: String,
       enum: ["Confirmed", "Finished"],
