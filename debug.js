565683ad558ecbd6c762cd6de09c564ca047c2ea
Rathanon Makaramanee <rathanon.makaramanee@gmail.com>
2025-04-19 16:09:58 +0700

    Added a function in debug.js to initiate total payment for all users (again).

--- debug.js
+++ debug.js
@@ -52,17 +52,19 @@ async function initiateTotalPaymentForAllUsers() {
       let sum = 0;
       for (const rent of rents){
         if (rent.totalPrice && rent.user_info) {
-          if (rent.user_info == user._id){
+          // console.log(rent.user_info + " " + user._id)
+          if (rent.user_info.equals(user._id)){
             sum += rent.totalPrice;
+            console.log(`User ${user._id} - adding $${rent.totalPrice}, total now ${sum}`);
           }
+          // console.log("loop " + sum)
         }
         else {
           console.log(`Skipped rent ${rent._id} due to missing data`);
         }
       }
-      user.totalPayment = sum;
-      await user.save();
-      console.log(`Updated rent ${rent._id}: ${rent.totalDays} days, $${rent.totalPrice}`);
+      await User.updateOne({ _id: user._id }, { totalPayment: sum });
+      console.log(`Updated user ${user._id}: $${sum}`);
     }
 
     await mongoose.disconnect();
