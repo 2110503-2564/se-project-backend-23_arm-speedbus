72b3fdfcaf10b2c60a8ecfea3995c47032cd5be7
Mantawit Ketphan <mantawit999@gmail.com>
2025-04-22 20:05:49 +0700

    add create rating

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -3,6 +3,7 @@ const Rating = require('../models/RatingModel');
 const User = require('../models/UserModel');
 const Provider = require('../models/ProviderModel');
 const Car = require('../models/CarModel');
+const Rent = require('../models/RentModel')
 
 // @desc     Get ratings for a car
 // @route    GET /api/v1/cars/:id/ratings
@@ -28,3 +29,30 @@ exports.getRatings = async (req, res) => {
       res.status(500).json({ success: false, msg: "Cannot fetch ratings" });
     }
   };
+
+// @desc    POST rating for car
+// route    POST /api/v1/cars/:id/ratings
+exports.createRating = async (req, res, next) => {
+  try {
+      const carId = req.params.id;
+      if (!carId) {
+        return res.status(404).json({ success : false, msg : "Cannot find the car."});
+      }
+
+      const rent = Rent.findOne({ user_info : req.user._id });
+      if (rent) {
+        return res.status(400).json({ success : false, msg: "You can rate only after you renting the car."});
+      }
+
+      const existRating = await Rating.find({ user_info : req.user._id });
+      if (existRating) {
+        return res.status(400).json({ success : false, msg: "You have already rate, You must update instead."});
+      }
+
+      const rating = await Rating.create(req.body);
+
+      res.status(200).json({ success : true, data : rating});
+  } catch (err) {
+    res.status(500).json({ success : false, msg: err});
+  }
+};
